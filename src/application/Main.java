8680716640905eb19606d9c6accb9f58aabc373f
Nick Stoffel <nickstoffel@ymail.com>
2018-04-20 09:12:40 -0500

    Got the GUI mostly working

--- src/application/Main.java
+++ src/application/Main.java
@@ -31,6 +31,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 
 public class Main extends Application {
@@ -49,16 +50,12 @@ public class Main extends Application {
 			gPane = new GridPane();
 			Scene scene = new Scene(gPane, 800, 600, Color.DARKTURQUOISE);
 			
-			gPane.setGridLinesVisible(true);
+			//gPane.setGridLinesVisible(true);
 			
 //			gPane.setMinHeight(bball.getNumTeams()*2+1);
 //			gPane.setMaxHeight(((Math.log(bball.getNumTeams()) / Math.log(2)) * 3) + 1);
 			
 			Label blank = new Label();
-			Label title = new Label();
-			title.setAlignment(Pos.TOP_CENTER);
-			title.setMinHeight(25);
-			title.setText("Tournament Bracket");
 			
 //			for(int i = 0; i < bball.getNumTeams()*2+1; i++)
 //				gPane.add(new Label(), 0, i);
@@ -67,8 +64,6 @@ public class Main extends Application {
 			
 			
 			slotSetup();
-			gPane.add(title, 1, 0);
-			GridPane.setHalignment(title, HPos.CENTER);
 			
 			
 			primaryStage.setScene(scene);
@@ -107,19 +102,28 @@ public class Main extends Application {
 	private void slotSetup() {
 		
 		int numTeams = bball.getNumTeams();
-		int numGames = (int)(Math.log(bball.getNumTeams()) / Math.log(2));
+		int numGames = (int)(Math.log(bball.getNumTeams()) / Math.log(2)+1);
 		teamLabels = new Label[numTeams][numGames];
 		teamScores = new TextField[numTeams][numGames];
 		submitButtons = new Button[numTeams/2][numGames];
+		int[][] num = new int[numTeams][numGames];
+		for(int i = 0; i < num.length; i++) {
+			for(int j = 0; j < num[i].length; j++) {
+				num[i][j] = 0;
+			}
+		}
+		
 		for(int i = 0; i < numTeams; i++) {
 			teamLabels[i][0] = new Label(bball.getTeam(i, 0));
 			teamScores[i][0] = new TextField();
+			teamLabels[i][0].setMinWidth(50);
+			teamLabels[i][0].setTextAlignment(TextAlignment.CENTER); // Doesn't work.
 			teamScores[i][0].setPromptText("Enter Score");
 			if(i%2 == 0)
 				submitButtons[i/2][0] = new Button("Submit");
 			for(int j = 1; j < numGames; j++) {
-				if(i/(Math.pow(2,j)) >= 0) {
-					teamLabels[i][j] = new Label();
+				if(i < numTeams/(j+1)) {
+					teamLabels[i][j] = new Label("Winner Game ");
 					teamScores[i][j] = new TextField();
 					if(i%2 == 0)
 						submitButtons[i/2][j] = new Button("Submit");
@@ -127,10 +131,11 @@ public class Main extends Application {
 			}
 		}
 		
+		
 		for(int i = 0; i < numTeams; i+=2) {
-			for(int j = 0; j < numGames; i++) {
+			for(int j = 0; j < numGames-1; j++) {
 				if(teamLabels[i+1][j] != null) {
-					gPane.add(new Label(), (j*4)+3, i*4);
+					gPane.add(new Label("Blank "), (j*4)+3, i*4);
 					gPane.add(teamLabels[i][j], (j*4)+1, (i*4)+1);
 					gPane.add(teamScores[i][j], (j*4)+2, (i*4)+1);
 					if(i%2 == 0)
@@ -140,6 +145,7 @@ public class Main extends Application {
 				}
 			}
 		}
+//		gPane.add(teamLabels[0][numGames-1],1,1);
 		
 	}
 }
